426a4044bbc83e91a78bdfe5e20f6306c42715cc
Stephen Suyanka <stepkans@gmail.com>
2024-07-27 12:10:58 -0700

    Add detailson Order table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -22,13 +22,13 @@ CREATE TABLE Customers (
 
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
-    FOREIGN KEY(customer_id) REFERENCES Customers (customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 
 CREATE TABLE Order_Details (
     order_details_id INT PRIMARY KEY,
-    FOREIGN KEY(order_id) REFERENCES Orders (order_id),
-    FOREIGN KEY(book_id) REFERENCES Books (book_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     order_date DATE
 );
